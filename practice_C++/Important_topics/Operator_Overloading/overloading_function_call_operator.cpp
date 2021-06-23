d5d97d826f100a8fe797b4198fd99e96e9aaa04c
Gurpreet Sarangal <gurpreetsarangal7@gmail.com>
2021-06-23 19:57:23 +0530

    added some data conversion program

--- practice_C++/Important_topics/Operator_Overloading/overloading_function_call_operator.cpp
+++ practice_C++/Important_topics/Operator_Overloading/overloading_function_call_operator.cpp
@@ -1,19 +1,21 @@
+// this is a special type of operator overloading 
 #include<iostream>
 using namespace std;
 class xyz
 {
     public:
-    int operator()(int num)
+    int operator()(int num)// this functions must be a member functions of class or complier will generate an error
     {
         int ch = num* num* num;
         return ch;
     }
 };
+
 int main()
 {
     xyz cube;
     int a = 5, res;
-    res = cube(a);
+    res = cube(a);// cube.operator()(a);
     cout<<res;
     return 0;
 }
